aa7cd3fb25652b5a950a8c561ced7ddeb11b15c2
Yaşar BAYKUŞ <byyasar@gmail.com>
2023-04-03 15:33:43 +0300

    oled calisti

--- config.h
+++ config.h
@@ -21,21 +21,15 @@
 
 //A VE B kanalı çok önemli değil, eğer yanlış bağlarsan bunları yer değiştir.
 #ifdef ENCODER_ENABLE
-
 #define ENCODER_RESOLUTION 4
 #endif
 
-
-//#define MY_I2C_ADDRESS (0x3C << 1)
-
 #ifdef OLED_ENABLE
+#define OLED_DISPLAY_WIDTH 128
+#define OLED_DISPLAY_HEIGHT 64
 #define I2C_DRIVER I2CD1
 #define I2C1_SCL_PIN B6
 #define I2C1_SDA_PIN B7
-#define OLED_BRIGHTNESS 128
-#define OLED_DISPLAY_ADDRESS 0x3C
-//#define I2C1_I2C_ADDRESS 0xBC
-
-//#define OLED_FONT_H "keyboards/mlego/m65/lib/glcdfont.c"
+#define OLED_DISPLAY_ADDRESS 0x3C    //#define OLED_FONT_H "keyboards/mlego/m65/lib/glcdfont.c"
 #endif
 
